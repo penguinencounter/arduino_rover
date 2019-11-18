818823c35ea40c2b38ead98d07027ebb1f7557af
Miles <miles.h.lin@gmail.com>
2019-11-17 16:42:29 -0800

    upd

--- rover.ino
+++ rover.ino
@@ -7,25 +7,45 @@ bool senseBlack = false;
 
 
 void setup() {
-    setThreshold(150);
+    setThreshold(25);
+    Serial.begin(9600);
+    while (!Serial) {
+       ; // wait for serial port to connect. Needed for native USB
+     }
 }
 
 void loop() {
+    char text[30] = {};
+    
+    readSensors();
+    sensorsToBools();
+    sprintf(text, "%d %d, %d %d, %d %d\n", sensors[0], sensorsBits[0], sensors[1], sensorsBits[1], sensors[2], sensorsBits[2]);
+    Serial.print(text);
     if (!senseBlack) {
-        readSensors();
-        sensorsToBools();
-        if (sensorsBits[0] && sensorsBits[2]) {
-            senseBlack = true;
-            return;
+
+        if (!sensorsBits[0] && !sensorsBits[1] && !sensorsBits[2]) {
+            move(64, 64);
         }
-        if (sensorsBits[0]) {
-            move(128, 0);
-        } else if (sensorsBits[2]) {
-            move(0, 128);
+        else if (sensorsBits[0] && !sensorsBits[2]) {
+            move(255, 0);
+            while (sensorsBits[0]) {
+              readSensors();
+              sensorsToBools();
+              delay(200);         
+           }
+        }
+        else if (sensorsBits[2] && !sensorsBits[0]) {
+            move(0, 255);
+            while (sensorsBits[2]) {
+              readSensors();
+              sensorsToBools();
+              delay(200);         
+           }
         } else {
-            move(128, 128);
+            stopAll();
         }
-        
+    } else {
+      
     }
     delay(100);
 }
